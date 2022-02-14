d91648a4b1e8b49c013557cce5471f1a3972a67f
yizaochen <yizaochen@gmail.com>
2022-02-14 23:04:34 +0800

    Reorganize avgcrd_fitdcd.py

--- notebooks/trajectory/avgcrd_fitdcd.py
+++ notebooks/trajectory/avgcrd_fitdcd.py
@@ -1,5 +1,8 @@
-import imp
+"""
+Reference: ./deprecated/1_make_noh_avg_fit_dcd.ipynb
+"""
 from trajectory.crd_dcd import FoldersBuilder
+
 
 rootfolder = '/home/ytcdata/traj/'
 suffix = 'before_clean'
@@ -8,4 +11,4 @@ host_lst = ['pnas_dna']
 for host in host_lst:
     builder = FoldersBuilder(rootfolder, suffix, host)
     builder.initialize_folders()
-    builder.copy_input_gro_xtc()
+    builder.copy_input_gro_xtc() # This should be replaced when the locations of input gro and xtc change
--- trajectory/trajectory/crd_dcd.py
+++ trajectory/trajectory/crd_dcd.py
@@ -1,8 +1,7 @@
 from os import path
 from miscell.file_util import check_dir_exist_and_make, check_file_exist, copy_verbose
 
-class FoldersBuilder:
-
+class AvgcrdFitdcd:
     def __init__(self, rootfolder, suffix, host):
         self.rootfolder = rootfolder # '/home/ytcdata/traj/'
         self.suffix = suffix # 'test', 'before_clean', 'after_clean'
@@ -24,6 +23,53 @@ class FoldersBuilder:
         self.strand1_pdb = path.join(self.mkcrd_folder, 'strand1.pdb')
         self.strand2_pdb = path.join(self.mkcrd_folder, 'strand2.pdb')
 
+    def main(self, exe_build_folder, exe_vmd_check_input_gro_xtc, exe_setup_pdb_dcd, exe_pdb2crd, exe_remove_hydrogen, exe_make_avg_crd, exe_make_fit_dcd, exe_vmd_check_output_crd_dcd):
+        self.build_folder(exe_build_folder)
+        self.vmd_check_input_gro_xtc(exe_vmd_check_input_gro_xtc)
+        self.setup_pdb_dcd(exe_setup_pdb_dcd)
+        self.pdb2crd(exe_pdb2crd) # Make CRD (split two strands, then combine) 
+        self.remove_hydrogen(exe_remove_hydrogen) # Make CRD and DCD without hydrogen atoms
+        self.make_avg_crd(exe_make_avg_crd) # Make Average CRD 
+        self.make_fit_dcd(exe_make_fit_dcd) # fitting no-H dcd to average crd
+        self.vmd_check_output_crd_dcd(exe_vmd_check_output_crd_dcd)
+
+    def build_folder(self, exec):
+        if exec:
+            agent = FoldersBuilder(self.rootfolder, self.suffix, self.host)
+            agent.initialize_folders()
+            agent.copy_input_gro_xtc() # This should be replaced when the locations of input gro and xtc change
+
+    def vmd_check_input_gro_xtc(self, exec):
+        if exec:
+            print(f'vmd -gro {self.npt4_gro} {self.raw_xtc}')
+
+    def setup_pdb_dcd(self, exec):
+        if exec:
+            pass
+
+    def pdb2crd(self, exec):
+        if exec:
+            pass
+
+    def remove_hydrogen(self, exec):
+        if exec:
+            pass
+
+    def make_avg_crd(self, exec):
+        if exec:
+            pass
+
+    def make_fit_dcd(self, exec):
+        if exec:
+            pass
+
+    def vmd_check_output_crd_dcd(self, exec):
+        if exec:
+            pass
+
+    
+class FoldersBuilder(AvgcrdFitdcd):
+
     def initialize_folders(self):
         for folder in [self.rootfolder, self.host_folder, self.suffix_folder, self.aa_folder, self.heavy_folder, self.inp_folder, self.dat_folder, self.mkcrd_folder]:
             check_dir_exist_and_make(folder)
@@ -39,6 +85,4 @@ class FoldersBuilder:
         copy_verbose(old_gro, self.npt4_gro)
         copy_verbose(old_xtc, self.raw_xtc)
 
-    def vmd_check_gro_xtc(self):
-        cmd = f'vmd -gro {self.npt4_gro} {self.raw_xtc}'
-        print(cmd)
+    
